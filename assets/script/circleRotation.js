b3c9b014ee29b18613508a526ba2e9265a709bb1
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-05-09 13:10:29 -0300

    Animando o Circle da Home

---
+++ assets/script/circleRotation.js
@@ -0,0 +1,13 @@
+const circleMiriam = document.querySelector('#circleMiriam');
+
+let  angle = 0;
+
+export function circleRotation () {
+    // incrementa o angulo
+    angle += .3;
+
+    circleMiriam.style.transform = `rotate(${angle}deg)`
+
+    requestAnimationFrame(circleRotation)
+    console.log('funcionou')
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+import { circleRotation } from "./assets/script/circleRotation.js";
+
+circleRotation();
