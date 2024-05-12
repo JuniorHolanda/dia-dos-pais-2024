c36eada31f2d8c27977ac6721fbeea6894905c3e
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-05-11 23:59:32 -0300

    ajustes e nova paleta de cores

---
+++ assets/script/carousel.js
@@ -0,0 +1,15 @@
+// referencia o container com os itens do carrossel
+const containerForCarousel = document.querySelector('#containerForCarousel');
+
+//referencia as div que contem o carrossel
+const listCarousel = document.querySelectorAll('.carousel');
+
+//referencia as imagens do carroussel
+const listImgCarousel = document.querySelectorAll('.carousel-item');
+
+//move os itens listCarousel para a esquerda
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,21 +1,8 @@
+//importa a função do circulo girando na primeira página
 import {circleRotation} from './assets/script/circleRotation.js'
 
-const containerForCarousel = document.getElementById('containerForCarousel');
-let position = 0;
-const itemWidth = containerForCarousel.firstElementChild.clientWidth;
-const totalItems = containerForCarousel.childElementCount;
+//duplicando os itens do carrossel para o loop infinito
+let copy = document.querySelector("#containerForCarousel").cloneNode(true);
+document.querySelector(".weapper-carousel").appendChild(copy);
 
-function moveCarousel() {
-    position -= 1; // Move o carrossel para a esquerda
-    containerForCarousel.style.transform = `translateX(${position}px)`;
-
-    // Verifica se o carrossel alcançou o fim e reinicia se necessário
-    if (Math.abs(position) >= itemWidth) {
-        containerForCarousel.appendChild(containerForCarousel.firstElementChild.cloneNode(true)); // Clonar o primeiro elemento
-        containerForCarousel.removeChild(containerForCarousel.firstElementChild); // Remover o primeiro elemento original
-        position += itemWidth;
-    }
-
-    // Solicita a próxima animação
-    requestAnimationFrame(moveCarousel);
-}
+console.log('hello')
